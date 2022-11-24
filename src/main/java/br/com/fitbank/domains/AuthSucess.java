dea20000572c484759a90a54c96f0644b1a2bc4b
renan <renan@easyc.com.br>
2022-11-24 14:13:24 -0300

    test OSC

--- src/main/java/br/com/fitbank/domains/AuthSucess.java
+++ src/main/java/br/com/fitbank/domains/AuthSucess.java
@@ -1,10 +1,11 @@
 package br.com.fitbank.domains;
 
 import java.io.Serializable;
+import java.time.Instant;
 
 public class AuthSucess implements Serializable {
     private String access_token;
-    private String expire_at;
+    private Instant expire_at;
 
     public AuthSucess() { }
     public AuthSucess(String access_token, String expire_at) {
@@ -18,10 +19,13 @@ public class AuthSucess implements Serializable {
     public void setAccess_token(String access_token) {
         this.access_token = access_token;
     }
-    public String getExpire_at() {
+    public Instant getExpire_at() {
         return expire_at;
     }
-    public void setExpire_at(String expire_at) {
+    public void setExpire_at(Instant expire_at) {
         this.expire_at = expire_at;
+    }
+    public void setExpire_at(String expire_at) {
+        this.expire_at = Instant.parse(expire_at);
     }
 }
